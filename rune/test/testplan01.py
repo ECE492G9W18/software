6c8f20b743c7233053449df692d898e2854102ba
Robomaster <robomaster@ubuntu-16.04>
2018-03-07 15:23:18 -0700

    Modified testplan2 to make it show all the output.

--- rune/test/testplan01.py
+++ rune/test/testplan01.py
@@ -8,12 +8,12 @@ from mnist_deep_estimator import *
 
 class Test(unittest.TestCase):
     def test_recog(self):
-        img = cv2.imread("1_/_00003.png", cv2.IMREAD_GRAYSCALE)
+        img = cv2.imread("7_/_00038.png", cv2.IMREAD_GRAYSCALE)
         img = cv2.resize(img, dsize=(28,28), interpolation=cv2.INTER_CUBIC)
         test_data = (255 - img.reshape((1, img.shape[0] * img.shape[1])) )/ 255.
 
         recognizer = numberRecognizer(model_dir="../model/model.ckpt")
-        self.assertEqual(recognizer.predict(test_data)[0][0],1)
+        self.assertEqual(recognizer.predict(test_data)[0][0],7)
         print(recognizer.predict(test_data))
     
 if __name__ == '__main__':
--- rune/test/testplan02.py
+++ rune/test/testplan02.py
@@ -8,10 +8,11 @@ from preprocess import cell_redundancy_removal
 class Test(unittest.TestCase):
     def test_removal(self):
         A = ((489.4820556640625, 276.8758544921875), (34.9864387512207, 76.87226867675781), -60.68999481201172)
-        B = ((489.4820556640625, 276.8758544921875), (34.9864387512207, 76.87226867675781), -60.68999481201172)
+        B = ((488, 274), (33, 74), -55)
         rects= [A,B]
              
         self.assertEqual(cell_redundancy_removal(rects, rects, rects)[1],[A])
+        print(cell_redundancy_removal(rects, rects, rects))
         
     
 if __name__ == '__main__':
